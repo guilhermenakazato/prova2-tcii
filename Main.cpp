37a5cff5028c1efe21378f80fb4f533b111c5347
guilhermenakazato <guilherme.fernandes23903@gmail.com>
2025-07-08 19:10:16 -0400

    Adding final tests

--- Main.cpp
+++ Main.cpp
@@ -104,24 +104,44 @@ main()
 
   ///////////////////////////////////////////////////movimento
   using VA = ElementAttribute<Color, Position, Intensity>;
-  using TA = ElementAttribute<Color>;
+  using TA = ElementAttribute<Color, Position, Intensity>;
   using MA = MeshAttribute<VA, TA>;
    
   auto ma2 = MA::New(std::move(ma->vertexAttributes()), *mesh);
-
+  
   auto n = mesh->data().triangleCount();
   for (decltype(n) i = 0; i < n; ++i) {
-      ma2->setTriangleAttributes(i, Color{ 1.0, 1.0f, 1.0f });
+    ma2->setTriangleAttributes(i, Color{ 1.0, 1.0f, 1.0f }, Position{0.1f,0.2f,0.3f}, Intensity{0.1f});
+  }
+  
+  
+  std::cout << "------------------------------- movement\n";
+  std::cout << "movement from ma to ma2 (vertices): \n";
+  for (auto i = 0; i < 2; i++) {
+    std::cout << "at pos " << i << ":\n\t";
+    std::cout << "vertex[ma2](color): " << ma2->vertexAttribute<0>(i) << '\n';
+    std::cout << "\tvertex[ma2](position): " << ma2->vertexAttribute<1>(i) << '\n';
+    std::cout << "\tvertex[ma2](intensity): " << ma2->vertexAttribute<2>(i) << '\n';
+    std::cout << "\ttriangle[ma2](color): " << ma2->triangleAttribute<0>(i) << '\n';
+    std::cout << "\ttriangle[ma2](position): " << ma2->triangleAttribute<1>(i) << '\n';
+    std::cout << "\ttriangle[ma2](intensity): " << ma2->triangleAttribute<2>(i) << '\n';
   }
 
-  std::cout << "------------------------------- movement\n";
-  std::cout << "movement from ma to ma2: \n";
+  auto ma3 = MA::New(*mesh, std::move(ma->triangleAttributes()));
+  auto m = mesh->data().vertexCount();
+  for(decltype(n) i = 0; i < m; ++i) {
+    ma3->setVertexAttributes(i, Color{0.5f, .5f, .5f}, Position{.7f, .7f, .7f}, Intensity{1.5f});
+  }
+  
+  std::cout << "movement from ma to ma3 (triangles): \n";
   for (auto i = 0; i < 2; i++) {
       std::cout << "at pos " << i << ":\n\t";
-      std::cout << "vertex[ma2](color): " << ma2->vertexAttribute<0>(i) << '\n';
-      std::cout << "\tvertex[ma2](position): " << ma2->vertexAttribute<1>(i) << '\n';
-      std::cout << "\tvertex[ma2](intensity): " << ma2->vertexAttribute<2>(i) << '\n';
-      std::cout << "\ttriangle[ma2](color): " << ma2->triangleAttribute<0>(i) << '\n';
+      std::cout << "vertex[ma3](color): " << ma3->vertexAttribute<0>(i) << '\n';
+      std::cout << "\tvertex[ma3](position): " << ma3->vertexAttribute<1>(i) << '\n';
+      std::cout << "\tvertex[ma3](intensity): " << ma3->vertexAttribute<2>(i) << '\n';
+      std::cout << "\ttriangle[ma3](color): " << ma3->triangleAttribute<0>(i) << '\n';
+      std::cout << "\ttriangle[ma3](position): " << ma3->triangleAttribute<1>(i) << '\n';
+      std::cout << "\ttriangle[ma3](intensity): " << ma3->triangleAttribute<2>(i) << '\n';
   }
   std::cout << "------------------------------- end of movement\n";
   ///////////////////////////////////////////////////movimento
@@ -134,13 +154,13 @@ main()
   auto triangle = Triangle::New(*mesh);
 
   vertex->setVertexAttributes(0, Color{0.01, 0.02, 0.03}, Position{0.1, 0.2, 0.3}, Intensity{0.123f});
-  triangle->setTriangleAttribute<0>(0, Color{0.23, 0.09, 0.05});
+  triangle->setTriangleAttributes(0, Color{0.23, 0.09, 0.05}, Position{0.3f, 0.4f, 0.5f}, Intensity{0.2f});
     
   std::cout << "------------------------------- void\n";
   std::cout << "===vertex\n";
-  std::cout << "Atributos do vertice: " << vertex->vertexAttribute<0>(0) << "," << vertex->vertexAttribute<1>(0) << "\n";
+  std::cout << "Atributos do vertice: " << vertex->vertexAttribute<0>(0) << "," << vertex->vertexAttribute<1>(0) << "," << vertex->vertexAttribute<2>(0) << "\n";
   std::cout << "===triangle\n";
-  std::cout << "Atributos do triangulo: " << triangle->triangleAttribute<0>(0) << "\n";
+  std::cout << "Atributos do triangulo: " << triangle->triangleAttribute<0>(0) << "," << triangle->triangleAttribute<1>(0) << "," << triangle->triangleAttribute<2>(0) << "\n";
   std::cout << "------------------------------- void\n";
 ///////////////////////////////////////////////////void
 
